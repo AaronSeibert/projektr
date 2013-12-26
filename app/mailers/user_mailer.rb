ef1c9af18ff774b456996a6ae2e082f32f9e1aef
Aaron Seibert <aaron@binaryitsystems.com>
2013-12-26 16:18:03 -0500

    Added env switch to mailer for url

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,21 @@
 class UserMailer < ActionMailer::Base
-  default from: "noreply@automated.binaryitsystems.com"
-  
+  Rails.env.production?
+    default from: "noreply@automated.binaryitsystems.com"
+  Rails.env.development?
+    default from: "noreply@sandbox11314.mailgun.org"
+  Rails.env.staging?
+    default from: "noreply@sandbox11314.mailgun.org"
   
   
   def welcome_email(user)
+    
+  Rails.env.production?
+    @url = 'http://172.16.25.150:3000'
+  Rails.env.development?
+    @url = 'http://172.16.25.150:3000'
+  Rails.env.staging?
+    @url = 'http://still-beach-8899.herokuapp.com'
+    
     @user = user
     @url  = 'http://172.16.25.150:3000'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
