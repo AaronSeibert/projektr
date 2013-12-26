596177d6bc95c41ced57e311346210d64fa5d972
Aaron Seibert <aaron@binaryitsystems.com>
2013-12-26 15:34:13 -0500

    Added tenant to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :email => 'aaron@binaryitsystems.com', :password => 'password', :password_confirmation => 'password', :superuser => true
+user = User.create! :email => 'aaron@binaryitsystems.com', :password => 'password', :password_confirmation => 'password', :superuser => true, :tenant_id => 1
 user.toggle!(:admin)
 puts 'New user created: ' << user.email
+
+puts 'SETTING UP DEFAULT TENANT'
+tenant = Tenant.create! :name => 'Binary IT Systems'
+puts 'New tenant created: ' << tenant.name
