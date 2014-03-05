b0367d76b2b71cc44c4e52a68b2053ee99e8c0ae
Aaron Seibert <aaron@binaryitsystems.com>
2014-03-05 14:09:05 -0500

    Closes #22

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -18,10 +18,15 @@ class ClientsController < ApplicationController
   def create
     @client = Client.new(client_params)
     
-    if @client.save
-      redirect_to clients_url
-    else
-      render 'new'
+    respond_to do |format|
+      if @client.save
+        format.html { redirect_to clients_url, :success => "Client created successfully!"}
+        format.json { render action: 'show', status: :created, location: @client }
+      else
+        format.html { render action: 'new' }
+        format.js   { render action: 'new' }
+        format.json { render json: @client.errors, status: :unprocessable_entity }
+      end
     end
   end
   
